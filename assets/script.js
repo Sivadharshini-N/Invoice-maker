a6f9e4dfcec6795a85eb083a7d3ca85485c1ad26
Sivadharshini-N <nsivadharshini1@gmail.com>
2024-08-05 12:35:31 +0530

    Final total

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,13 @@ function additem() {
     console.log("Adding new item row");
     var newRow = document.createElement('tr');
 
-    newRow.innerHTML = `
-        <td><textarea name="" id="" placeholder="Enter item " rows="3"></textarea><br><br><input type="text" placeholder="HSN/SAC"></td>
+    newRow.innerHTML = `<td><textarea name="" id="" placeholder="Enter item " rows="3"></textarea><br><br><input type="text" placeholder="HSN/SAC"></td>
         <td><input type="number" class="qty" value="1" onchange="calculateTotals()"></td>
-        <td><input type="number" oninput="calculateTotals()"  value="0.00" step="0.01" class="rate"></td>
+        <td><input type="number" oninput="calculateTotals()"  value="0.00" class="rate"></td>
         <td><input type="number" class="sgst"  value="0"  oninput="calculateTotals()" ><br><span class="csgst"></span></td>
         <td><input type="number" class="cgst" value="0" oninput="calculateTotals()"><br><p class="ccgst"></p></td>
         <td><input type="number" class="cess" value="0" ><br><p class="ccess"></p></td>
-        <td><p class="total"></p></td> 
-        
-    `;
+        <td><p class="total"></p></td>`;
 
     var tableBody = document.getElementById('tablebody');
     tableBody.appendChild(newRow);
@@ -34,6 +31,15 @@ var CCESS = document.getElementsByClassName("ccess");
 var TOTAL = document.getElementsByClassName("total");
 
 
+var wholesub = document.getElementById("wholesub");
+var wholesgst = document.getElementById("wholesgst");
+var wholecgst = document.getElementById("wholecgst");
+var wholetotal = document.getElementById("wholetot");
+
+window.onload = function()
+{
+    calculateTotals();
+}
 
 
 
@@ -72,9 +78,33 @@ function calculateTotals() {
         
         
     }
+    wholetot();
       
 }
 
 
+function wholetot()
+{
+    
+    var sum=0;
+    var sumsgst= 0;
+    var sumcgst = 0;
+    var sumtot=0;
+    for(let i=0;i<TOTAL.length;i++)
+    {
+        sum+=parseInt(TOTAL[i].innerHTML);
+        sumsgst+=parseInt(CSGST[i].innerHTML);
+        sumcgst+=parseInt(CCGST[i].innerHTML);
 
+        
+        
+        
+    }
+    sumtot = sum+sumsgst+sumcgst;
+    wholesub.innerHTML=sum;
+    wholesgst.innerHTML=sumsgst;
+    wholecgst.innerHTML=sumcgst;
+    wholetotal.innerHTML=sumtot;
+
+}
    
